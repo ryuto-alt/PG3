ffd18c6d422c2a6f249cb125b17fef27f45d3ac8
ryuto-alt <146914455+ryuto-alt@users.noreply.github.com>
2025-01-29 14:38:07 +0900

    3-1完成

--- main.cpp
+++ main.cpp
@@ -1,62 +1,34 @@
 #include <stdio.h>
+#include <iostream>
+#include <list>
 
-// 親クラス
-class Shape
-{
-public:
-    virtual void Size() = 0;
-    virtual void Draw() = 0;
+using namespace std;
 
-protected:
-    float size;
-};
+int main() {
 
-// 円クラス
-class Circle : public Shape
-{
-public:
-    void Size() override;
-    void Draw() override { printf("円の面積: %f\n", size); }
-};
+    list<const char*> rosen = { "田端", "日暮里", "西日暮里" };
 
-// 四角クラス
-class Rectangle : public Shape
-{
-public:
-    void Size() override;
-    void Draw() override { printf("矩形の面積: %f\n", size); }
+    // 駅を追加
+    rosen.push_back("上野");
+    rosen.push_back("東京");
 
-private:
-    float width, height;
-};
+    // 1回目の表示
+    for (list<const char*>::iterator itr = rosen.begin();
+        itr != rosen.end();
+        itr++)
+    {
+        cout << *itr << endl;
+    }
 
-void Circle::Size()
-{
-    float radius = 5.0f;
-    printf("円の半径: %f\n", radius);
-    size = radius * radius * 3.14f; // πr^2
-}
+    cout << "=================" << endl;
 
-void Rectangle::Size()
-{
-    width = 5.0f;
-    height = 10.0f;
-    printf("矩形の幅: %f, 高さ: %f\n", width, height);
-    size = width * height; // 幅×高さ
-}
-
-int main(void)
-{
-    Shape* shapes[2] = { new Circle(), new Rectangle() };
-
-    shapes[0]->Size();
-    shapes[1]->Size();
-
-    shapes[0]->Draw();
-    shapes[1]->Draw();
-
-    delete shapes[0];
-    delete shapes[1];
+    // 2回目の表示
+    for (list<const char*>::iterator itr = rosen.begin();
+        itr != rosen.end();
+        itr++)
+    {
+        cout << *itr << endl;
+    }
 
     return 0;
 }
