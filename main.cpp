0740c16ade73dbc0a1c09a2536c8f360d9c87831
ryuto-alt <146914455+ryuto-alt@users.noreply.github.com>
2025-01-29 14:36:48 +0900

    2-4完成

--- main.cpp
+++ main.cpp
@@ -1,37 +1,62 @@
-#include <cstdio>
-using namespace std;
+#include <stdio.h>
 
-class Animal {
+// 親クラス
+class Shape
+{
 public:
-    virtual void sound() const = 0;  
+    virtual void Size() = 0;
+    virtual void Draw() = 0;
+
+protected:
+    float size;
 };
 
-
-class Dog : public Animal {
+// 円クラス
+class Circle : public Shape
+{
 public:
-    void sound() const override {
-        printf("イヌ：ワン!\n");
-    }
+    void Size() override;
+    void Draw() override { printf("円の面積: %f\n", size); }
 };
 
-
-class Cat : public Animal {
+// 四角クラス
+class Rectangle : public Shape
+{
 public:
-    void sound() const override {
-        printf("ネコ:ニャー!\n");
-    }
+    void Size() override;
+    void Draw() override { printf("矩形の面積: %f\n", size); }
+
+private:
+    float width, height;
 };
 
-int main() {
-    Animal* animal1 = new Dog();  
-    Animal* animal2 = new Cat();  
+void Circle::Size()
+{
+    float radius = 5.0f;
+    printf("円の半径: %f\n", radius);
+    size = radius * radius * 3.14f; // πr^2
+}
 
-    animal1->sound();  
-    animal2->sound();  
+void Rectangle::Size()
+{
+    width = 5.0f;
+    height = 10.0f;
+    printf("矩形の幅: %f, 高さ: %f\n", width, height);
+    size = width * height; // 幅×高さ
+}
 
-    
-    delete animal1;
-    delete animal2;
+int main(void)
+{
+    Shape* shapes[2] = { new Circle(), new Rectangle() };
+
+    shapes[0]->Size();
+    shapes[1]->Size();
+
+    shapes[0]->Draw();
+    shapes[1]->Draw();
+
+    delete shapes[0];
+    delete shapes[1];
 
     return 0;
 }
