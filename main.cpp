cf1c793a3ffd3bf28e8e4b7c905e5ff1b3cb424b
ryuto-alt <146914455+ryuto-alt@users.noreply.github.com>
2025-02-07 22:45:02 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,34 +1,49 @@
 #include <stdio.h>
-#include <iostream>
 #include <list>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-
-    list<const char*> rosen = { "田端", "日暮里", "西日暮里" };
-
-    // 駅を追加
-    rosen.push_back("上野");
-    rosen.push_back("東京");
-
-    // 1回目の表示
-    for (list<const char*>::iterator itr = rosen.begin();
-        itr != rosen.end();
-        itr++)
-    {
-        cout << *itr << endl;
+// リスト全体を表示する関数
+void printList(const list<const char*>& l) {
+    for (auto station : l) {
+        cout << station << endl;
     }
+}
 
-    cout << "=================" << endl;
+int main(void) {
 
-    // 2回目の表示
-    for (list<const char*>::iterator itr = rosen.begin();
-        itr != rosen.end();
-        itr++)
-    {
-        cout << *itr << endl;
+    list<const char*> eki_list{
+        "Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani",
+        "Nippori", "Tabata", "Komagome", "Sugamo", "Otsuka", "Ikebukuro", "Mejiro",
+        "Takadanobaba", "Sin-Okubo", "Shinjuku", "Yoyogi", "Harajuku", "Shibuya",
+        "Ebisu", "Meguro", "Gotanda", "Osaki", "Sinagawa", "Tamachi", "Hamamatsucho",
+        "Shimbashi", "Yurakucho"
+    };
+
+    // 1970年のリスト表示
+    printf("1970年\n");
+    printList(eki_list);
+
+    // 2019年： "Tabata"の前に "Nishi-Nippori" を挿入
+    for (auto it = eki_list.begin(); it != eki_list.end(); ++it) {
+        if (*it == "Tabata") {
+            eki_list.insert(it, "Nishi-Nippori");
+            break;
+        }
     }
+    printf("\n2019年\n");
+    printList(eki_list);
+
+    // 2022年： "Tamachi" の前に "Takanawa Gateway" を挿入
+    for (auto it = eki_list.begin(); it != eki_list.end(); ++it) {
+        if (*it == "Tamachi") {
+            eki_list.insert(it, "Takanawa Gateway");
+            break;
+        }
+    }
+    printf("\n2022年\n");
+    printList(eki_list);
 
     return 0;
 }
